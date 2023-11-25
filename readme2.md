c61a16d8c4bb9803bdc2fb60e23cb3a655c61a14
Ayush Kumar Gupta <ayush.skg@gmail.com>
2023-11-25 20:40:45 +0530

    navbar add

---
+++ readme2.md
@@ -0,0 +1,3 @@
+<https://foamy-triangle-7b4.notion.site/Gweiland-Nextjs-Task-618a13bc6ac64807b79238e855eedda9>
+
+<https://bravaland.com/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/components/Navbar";
 import TopAnnouncement from "@/components/TopAnnouncement";
+import Carousel from "react-material-ui-carousel";
 
 export default function Home() {
   return (
     <>
       <TopAnnouncement />
+      <Navbar />
+      <Carousel></Carousel>
     </>
   );
 }
